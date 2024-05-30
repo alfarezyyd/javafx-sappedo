d556edc6c131ca7649c41460993a256d8d55cded
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-30 13:05:41 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module alfarezyyd.sappedo {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.web;
+
+  requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+  requires net.synedra.validatorfx;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+  requires eu.hansolo.tilesfx;
+  requires javafx.graphics;
+
+  opens alfarezyyd.sappedo to javafx.fxml;
+  exports alfarezyyd.sappedo;
+}
